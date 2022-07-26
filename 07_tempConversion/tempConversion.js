b820b16d70d95d341e181bd0ba06c2b2f008a1f7
Mark P <mj.poso18@gmail.com>
2022-07-27 00:05:01 +0800

    Finish Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,40 @@
-const ftoc = function() {
+function roundOff(number, places = 2) {
+  /*  Given `number = 101.2534`
+   *  Multiplying `number` by 10 moves the decimal to the right,
+   *  i.e. `(number * 10) == 1012.534`
+   *
+   *  The number of places the decimal moves depends on the
+   *  number of times `number` is multiplied by 10
+   *  i.e. 10 ** (places)
+   */
+  const factorPlaceValue = 10 ** places;
 
+  const result = Math.round(number * factorPlaceValue) / factorPlaceValue;
+  return result;
+}
+
+const ftoc = function (fahrenheit) {
+  /*  C = (F − 32) × 5/9
+   */
+
+  const resultFormula = (fahrenheit - 32) * (5 / 9);
+  const result = roundOff(resultFormula, 1);
+
+  return result;
 };
 
-const ctof = function() {
+const ctof = function (celsius) {
+  /*  F = (C × 9/5) + 32
+   */
 
+  const resultFormula = celsius * (9 / 5) + 32;
+  const result = roundOff(resultFormula, 1);
+
+  return result;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
