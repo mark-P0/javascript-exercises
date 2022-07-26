63d3ccdc68a0b2628f2488728692c5a460cce2b5
Mark P <mj.poso18@gmail.com>
2022-07-26 23:11:22 +0800

    Finish Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (
+    /*  */
+    !(typeof string === 'string' && typeof num === 'number') ||
+    !(num >= 0)
+  )
+    return 'ERROR';
 
+  return string.repeat(num);
 };
 
 // Do not edit below this line
