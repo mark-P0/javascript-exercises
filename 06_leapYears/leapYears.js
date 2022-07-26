31248fc854ff85e8137bdd81d13ed46ba66cef0f
Mark P <mj.poso18@gmail.com>
2022-07-26 23:48:48 +0800

    Finish Exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const a = year % 4 === 0;
+  const b = year % 100 === 0;
+  const c = year % 400 === 0;
 
+  /* prettier-ignore */
+  const isLeapYear = (
+    (a && !b) ||  // Leap year if div-by-4 and not div-by-100
+    (a && c)      // Leap year if div-by-4 and div-by-400
+  )
+
+  return isLeapYear;
 };
 
 // Do not edit below this line
