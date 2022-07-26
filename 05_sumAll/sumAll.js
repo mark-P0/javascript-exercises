0d81b179baab43aef39c5df6df1ccc5a80f5b7df
Mark P <mj.poso18@gmail.com>
2022-07-26 23:44:01 +0800

    Finish Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (lower, upper) {
+  /* Validation */
+  for (const arg of [lower, upper]) {
+    /* prettier-ignore */
+    if (!(
+      (typeof arg === 'number') &&
+      (arg >= 0)
+    )) return 'ERROR';
+  }
 
+  /* Swap arguments if not in order */
+  if (lower > upper) [lower, upper] = [upper, lower];
+
+  const numRange = Array.from(
+    { length: upper - lower + 1 },
+    (_, idx) => idx + lower
+  );
+
+  const sum = numRange.reduce((acmlSum, num) => acmlSum + num, 0);
+
+  return sum;
 };
 
 // Do not edit below this line
