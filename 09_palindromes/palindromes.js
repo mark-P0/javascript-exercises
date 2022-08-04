5bfbc0b42fe51325b3247186edb600356c9ea8ad
Mark P <mj.poso18@gmail.com>
2022-08-04 17:48:09 +0800

    Finish Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  if (typeof string !== 'string') {
+    throw `Argument ${string} is not a string!`;
+  }
 
+  string = string.toLowerCase().replace(/[^a-z]/g, '');
+  const result = string === string.split('').reverse().join('');
+  return result;
 };
 
 // Do not edit below this line
