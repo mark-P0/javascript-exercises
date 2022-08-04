acea391ab4b8461b1072f1a85278706f67eb0943
Mark P <mj.poso18@gmail.com>
2022-08-04 17:37:27 +0800

    Finish Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+function isNumeric(number) {
+  return +number === number;
+}
+
+const add = function (addend1, addend2) {
+  return addend1 + addend2;
 };
 
-const subtract = function() {
-	
+const subtract = function (minuend, subtrahend) {
+  return minuend - subtrahend;
 };
 
-const sum = function() {
-	
+const sum = function (addends) {
+  const result = addends.reduce((acmlSum, addend) => acmlSum + addend, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (factors) {
+  const product = factors.reduce((acmlProd, factor) => acmlProd * factor, 1);
+  return product;
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  const numArray = Array.from({ length: num }, (_, idx) => idx + 1);
+  const result = multiply(numArray);
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +37,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
