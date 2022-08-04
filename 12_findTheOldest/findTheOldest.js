c58ea3a26790da80efd2b9567ea4df2e2c8074f1
Mark P <mj.poso18@gmail.com>
2022-08-04 18:13:55 +0800

    Perform Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const currentYear = new Date().getFullYear();
 
+  const oldestPerson = people.reduce((lastPerson, nextPerson) => {
+    const { yearOfBirth: lastBirth, yearOfDeath: lastDeath = currentYear } =
+      lastPerson;
+    const { yearOfBirth: nextBirth, yearOfDeath: nextDeath = currentYear } =
+      nextPerson;
+
+    const [lastAge, nextAge] = [lastDeath - lastBirth, nextDeath - nextBirth];
+    return lastAge > nextAge ? lastPerson : nextPerson;
+  });
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
