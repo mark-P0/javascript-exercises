ec6ce63d6662afcfcbd77a4ae9b37c1078ebce1a
Mark P <mj.poso18@gmail.com>
2022-08-04 18:00:12 +0800

    Finish Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  /* prettier-ignore */
+  if (!(
+    n &&
+    Number.isInteger(+n)
+  )) throw `Argument "${n}" is not an integer!`
 
+  if (n <= 0) return 'OOPS';
+  if (n < 3) return 1;
+
+  let fibTerms = [1, 1];
+  for (let _ = 2; _ < n; _++) {
+    fibTerms[1] = fibTerms[1] + fibTerms[0];
+    fibTerms[0] = fibTerms[1] - fibTerms[0];
+  }
+  return fibTerms[1];
 };
+
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
